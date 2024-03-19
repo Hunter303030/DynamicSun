5ce145934b2f2156a5690b5e6aa4bb9be0085eac
Олег Друян <druian.oleg@yandex.ru>
2024-03-20 02:39:10 +0300

    ...

--- Controllers/WeatherController.cs
+++ Controllers/WeatherController.cs
@@ -1,74 +1,70 @@
-﻿using DynamicSun.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using DynamicSun.Models;
 using DynamicSun.Repository.Interfaces;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NPOI.SS.UserModel;
-using NPOI.Util;
 using NPOI.XSSF.UserModel;
-using NPOI.XWPF.UserModel;
 
-namespace DynamicSun.Controllers
+namespace DynamicSun.Controllers;
+
+public class WeatherController : Controller
 {
-    public class WeatherController : Controller
+    private readonly IWeatherRepository _weatherRepository;
+
+    private readonly Microsoft.AspNetCore.Hosting.IHostingEnvironment _env;
+
+    public WeatherController(IWeatherRepository weatherRepository, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
     {
+        _weatherRepository = weatherRepository;
+        _env = env;
+    }
 
-        private readonly IWeatherRepository _weatherRepository;
-        private readonly Microsoft.AspNetCore.Hosting.IHostingEnvironment _env;
+    public IActionResult List()
+    {
+        IEnumerable<Weather> weatherList = _weatherRepository.List();
+        return View("~/Views/Main.cshtml", weatherList);
+    }
 
-        public WeatherController(IWeatherRepository weatherRepository,
-                                 Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+    [HttpPost]
+    public IActionResult AddArchive(IEnumerable<IFormFile> fileArchive)
+    {
+        string dir = _env.ContentRootPath + "\\Archive";
+        foreach (IFormFile item in fileArchive)
         {
-            _weatherRepository = weatherRepository;
-            _env = env;
-        }
-
-        public IActionResult List()
-        {
-            var weatherList = _weatherRepository.List();
-
-            return View("~/Views/Main.cshtml", weatherList);
-        }
-
-        [HttpPost]
-        public IActionResult AddArchive(IEnumerable<IFormFile> fileArchive)
-        {
-            var dir = _env.ContentRootPath + "\\Archive";
-
-            foreach(var item in fileArchive)
+            IFormFile file = item;
+            using (FileStream target = new FileStream(Path.Combine(dir, item.FileName), FileMode.Create, FileAccess.Write))
             {
-                IWorkbook workbook;
-                IFormFile file = item;
-                using (FileStream fileStream = new FileStream(Path.Combine(dir,item.FileName), FileMode.Create, FileAccess.Write))
+                file.CopyTo(target);
+            }
+            IWorkbook workbook;
+            using (FileStream fileStream = new FileStream(dir + "/" + file.FileName, FileMode.Open, FileAccess.Read))
+            {
+                workbook = new XSSFWorkbook((Stream)fileStream);
+            }
+            int numberOfSheets = workbook.NumberOfSheets;
+            DataFormatter formatter = new DataFormatter();
+            for (int i = 0; i < numberOfSheets; i++)
+            {
+                ISheet sheet = workbook.GetSheetAt(i);
+                int numberOfRows = sheet.LastRowNum;
+                int numberOfCell = 12;
+                for (int r = 4; r < numberOfRows; r++)
                 {
-                    file.CopyTo(fileStream);                    
-                }
-
-                using (FileStream fileStream = new FileStream($"{dir}/{file.FileName}", FileMode.Open, FileAccess.Read))
-                {
-                    workbook = new XSSFWorkbook(fileStream);
-                }
-                int numberOfSheets = workbook.NumberOfSheets;
-                for (int i = 0; i < numberOfSheets; i++)
-                {
-                    ISheet sheet = workbook.GetSheetAt(i);
-                    int numberOfRows = sheet.LastRowNum;
-                    int numberOfCell = 12;
-
-                    for (int r = 4; r < numberOfRows; r++)
+                    for (int c = 0; c < numberOfCell; c++)
                     {
-                        for (int c = 0; c < numberOfCell; c++)
-                        {
-                            DataFormatter formatter = new DataFormatter();
-                            IRow row = sheet.GetRow(r);
-                            var cellValue =formatter.FormatCellValue(row.GetCell(c));
-                            
-                            Console.WriteLine(cellValue);
-                        }
+                        IRow row = sheet.GetRow(r);
+                        var cellValue = formatter.FormatCellValue(row.GetCell(c));
+
+                        Console.Write($"{cellValue} | ");
                     }
+                    Console.WriteLine();
                 }
             }
-            
-
-            return List();
         }
+        return List();
     }
 }
